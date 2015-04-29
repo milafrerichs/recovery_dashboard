751413260f1ba8a5c498b84616b499b469041293
mila.frerichs@gmail.com <mila.frerichs@gmail.com>
2015-04-29 14:59:20 +0200

    Automated commit at 2015-04-29 12:59:19 UTC by middleman-deploy 1.0.0

---
+++ javascripts/dashboard.js
@@ -0,0 +1,64 @@
+(function() {
+  var dashboard;
+
+  dashboard = angular.module("dashboard", ["openlayers-directive"]);
+
+  dashboard.controller("RecoveryDashboardCtrl", [
+    '$http', '$scope', function($http, $scope) {
+      var mapboxLayer, medicalLayer, roadsLayer;
+      mapboxLayer = {
+        name: 'Mapbox',
+        active: true,
+        opacity: 0.5,
+        source: {
+          type: 'TileJSON',
+          url: 'http://api.tiles.mapbox.com/v3/mapbox.geography-class.jsonp'
+        }
+      };
+      medicalLayer = {
+        name: 'Medical',
+        active: true,
+        source: {
+          type: 'GeoJSON',
+          url: 'data/medical.geojson'
+        }
+      };
+      roadsLayer = {
+        name: 'Roads',
+        active: true,
+        visible: false,
+        source: {
+          type: 'GeoJSON',
+          url: 'data/main_roads.geojson'
+        },
+        style: new ol.style.Style({
+          stroke: new ol.style.Stroke({
+            color: 'red',
+            width: 2
+          })
+        })
+      };
+      $scope.layerGroups = [
+        {
+          name: "Health",
+          layers: [medicalLayer]
+        }, {
+          name: "Infrastructure",
+          layers: [roadsLayer]
+        }
+      ];
+      return angular.extend($scope, {
+        defaults: {
+          scrollWheelZoom: false
+        },
+        nepal: {
+          lat: 27.7,
+          lon: 85.3,
+          zoom: 7
+        },
+        layers: [medicalLayer, roadsLayer, mapboxLayer]
+      });
+    }
+  ]);
+
+}).call(this);
