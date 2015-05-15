4042b10fccd9b2f2f82eef2ed6dbf13fba464941
Mila Frerichs <mila.frerichs@gmail.com>
2015-05-15 10:45:53 +0200

    Add code coverage to karma

--- karma.conf.js
+++ karma.conf.js
@@ -38,6 +38,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      '**/*.coffee': ['coverage'],
       '**/*.coffee': ['coffee']
     },
 
@@ -45,7 +46,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
